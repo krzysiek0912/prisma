8939ddf5f51f31d68364962d75637beb1d7def8f
Krzysztof Branecki <krzysztof.branecki@frontkom.com>
2022-12-31 11:48:49 +0100

    Add middleware to req body and change behavior 404 to json response

--- back-end-express/src/server.js
+++ back-end-express/src/server.js
@@ -1,7 +1,21 @@
 import express from 'express'
 
+const { PORT = 3000 } = process.env
 const app = express()
 
-app.listen(4100, () => {
-  console.log('listening on port 4100')
+app.use(express.json())
+
+app.post('/user/sign-in', (req, res) => {
+  console.log('The body is', req.body)
+  res.json({ hello: req.body })
+})
+
+app.all('*', (req, res) => {
+  const { method, originalUrl } = req
+  res
+    .status(404)
+    .json({ message: `Path ${originalUrl} for method ${method} not found :( ` })
+})
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
